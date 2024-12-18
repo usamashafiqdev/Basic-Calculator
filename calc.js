1abad4ec8d83ae56a4252b26a5c85f0fe7e132e9
usamashafiqdev <itusamashafiq@gmail.com>
2024-12-18 15:19:38 +0500

    intial commit

---
+++ calc.js
@@ -0,0 +1,16 @@
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function appendCharacter(character) {
+    document.getElementById('display').value += character;
+}
+
+function calculate() {
+    try {
+        const result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
